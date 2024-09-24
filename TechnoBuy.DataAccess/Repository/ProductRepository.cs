acc64d33720d3c1f74085f0e131d07f9525a2a4a
Uvejsii <136826106+Uvejsii@users.noreply.github.com>
2024-09-24 16:16:57 +0200

    Add project files.

---
+++ TechnoBuy.DataAccess/Repository/ProductRepository.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnoBuy.DataAccess.Data;
+using TechnoBuy.DataAccess.Repository.IRepository;
+using TechnoBuy.Models;
+
+namespace TechnoBuy.DataAccess.Repository
+{
+    public class ProductRepository : Repository<Product>, IProductRepository
+    {
+        private ApplicationDbContext _db;
+
+        public ProductRepository(ApplicationDbContext db) : base(db)
+        {
+            _db = db;
+        }
+
+        public void Update(Product product)
+        {
+            var productFromDb = _db.Products.Find(product.Id);
+
+            if (productFromDb != null)
+            {
+                productFromDb.Name = product.Name;
+                productFromDb.Description = product.Description;
+                productFromDb.Price = product.Price;
+                productFromDb.Brand = product.Brand;
+                productFromDb.CategoryId = product.CategoryId;
+                if (product.ImageUrl != null)
+                {
+                    productFromDb.ImageUrl = product.ImageUrl;
+                }
+            }
+        }
+    }
+}
