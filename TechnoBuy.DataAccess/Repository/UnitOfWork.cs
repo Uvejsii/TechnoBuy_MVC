2f59be668e28592b211e5cf26814c216e706c50f
Uvejsii <136826106+Uvejsii@users.noreply.github.com>
2024-09-24 16:49:32 +0200

    Implemented UnitOfWork

---
+++ TechnoBuy.DataAccess/Repository/UnitOfWork.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using TechnoBuy.DataAccess.Data;
+using TechnoBuy.DataAccess.Repository.IRepository;
+
+namespace TechnoBuy.DataAccess.Repository
+{
+    public class UnitOfWork : IUnitOfWork
+    {
+        private ApplicationDbContext _db;
+
+        public IProductRepository Product { get; private set; }
+        public ICategoryRepository Category { get; private set; }
+
+        public UnitOfWork(ApplicationDbContext db)
+        {
+            _db = db;
+            Product = new ProductRepository(_db);
+            Category = new CategoryRepository(_db);
+        }
+
+        public void Save()
+        {
+            _db.SaveChanges();
+        }
+    }
+}
--- TechnoBuyWeb/Program.cs
+++ TechnoBuyWeb/Program.cs
@@ -1,5 +1,7 @@
 using Microsoft.EntityFrameworkCore;
 using TechnoBuy.DataAccess.Data;
+using TechnoBuy.DataAccess.Repository;
+using TechnoBuy.DataAccess.Repository.IRepository;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +10,8 @@ builder.Services.AddControllersWithViews();
 
 builder.Services.AddDbContext<ApplicationDbContext>(options => 
     options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
+
+builder.Services.AddScoped<IUnitOfWork, UnitOfWork>();
 
 var app = builder.Build();
 
